50cab8da73e15c131129741e4bca16fbfc815b11
Paras Anand <speed1992@gmail.com>
2018-08-05 01:39:08 +0530

    Added functionalities

--- app.js
+++ app.js
@@ -1,4 +1,65 @@
-var fs = require('fs');
-var copydir = require('copy-dir');
+let fs = require('fs');
+const fse = require('fs-extra');
+var mkdirp = require('mkdirp');
+let folderPath = 'output/';
+let projectName, numberOfFiles;
+let jsonObj;
 
-console.log("Node modules loaded successfully!");
+async function CloneFiles() {
+  try {
+    await fse.copy('./input/', './output/')
+    console.log('Files cloned to output -> ok!');
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function GetProjectName() {
+  await fs.readdirSync(folderPath).forEach((file,index) => {
+    projectName = file;
+    if (index != 0) {
+      jsonObj += `,`;
+    }
+    jsonObj +='{\"projectName\":\"';
+    jsonObj += file;
+    jsonObj += '\",\"paths\":[';
+    console.log(projectName);
+    var newPath = folderPath + projectName + '/trunk/s9ml/cards/' ;
+    (async function() {
+      await MakeJSON_HTMLFileNames(newPath, projectName);
+    })()
+    
+  });
+  
+}
+async function MakeJSON_HTMLFileNames(newPath, projectName) {
+  fs.readdirSync(newPath).forEach((file, index) => {
+    if (!(index == 0)) {
+      jsonObj += `,`;
+    }
+    jsonObj += `{\"path\":\"${newPath}${file}\"}`;
+
+  });
+  jsonObj += "]}";
+}
+
+async function WriteJSON() {
+  await mkdirp('../front/json/', function (err) {
+    if (err) console.error(err)
+    else console.log('JSON directory created!')
+  });
+  await fs.writeFile('../front/json/data.json', jsonObj.toString(), (err) => {
+    if (err) throw err;
+    console.log('JSON saved!');
+  });
+}
+
+async function init() {
+  await CloneFiles();
+  jsonObj ='[';
+  await GetProjectName();
+  jsonObj +=']';
+  await WriteJSON();
+}
+
+init();
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors')
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var router = express.Router();
+
+var fs = require('fs');
+
+var projectStart = require('./app.js');
+
+router.post('/iframeData', function (req, res) { req.setTimeout(0); getData(req, res); });
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/', router);
+
+var port = 3000;
+app.listen(port);
+app.timeout = -1;
+
+console.log('Server listening @ localhost:' + port);
+
+getData = async function (req, res) {
+    res.send('ok');
+    var DOM1 = unescape(req.body.DOM);
+    //console.log(unescape(req.body.path));
+    var pathToWrite = req.body.path.split("/") ;
+    //console.log(pathToWrite);
+    pathToWrite = pathToWrite.splice(2);
+    pathToWrite = pathToWrite.join('/');
+
+    pathToWrite = "./" + decodeURIComponent(pathToWrite);
+    //console.log(pathToWrite);
+    await writeHTMLFile(pathToWrite,DOM1);
+}
+writeHTMLFile = async function(pathToWrite, DOM1){
+    fs.writeFile(pathToWrite.trim(), DOM1, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Contents written to : " + pathToWrite);
+    });
+}
